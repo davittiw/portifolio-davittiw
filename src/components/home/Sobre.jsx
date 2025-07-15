479b39b2b3a8d64a4238171c4444b36f392416b2
davittiw <lucctyte@gmail.com>
2025-07-15 12:28:03 -0300

    asdasd

--- src/components/home/Sobre.jsx
+++ src/components/home/Sobre.jsx
@@ -4,7 +4,7 @@ function Sobre() {
     const { t } = useTranslation();
 
     return(
-    <div className="w-80 sm:w-90 md:w-120 lg:w-120 h-40 mt-5 rounded-xl border border-gray-700 shadow-3xl">
+    <div className="w-80 sm:w-90 md:w-120 lg:w-120 h-60 mt-5 rounded-xl border border-gray-700 shadow-3xl">
         <div className="flex flex-col items-left justify-center p-3 md:p-4 lg:p-4">
             <h2 className="text-white text-lg font-bold">{t('About.title')}</h2>
             <p className="text-gray-300 text-sm mt-2">
